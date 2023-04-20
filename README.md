3fd961b7578bb2bcf6370a1cdd7a67f0b88e1af9
Nick Adams <nickolas.j.adams@gmail.com>
2023-04-19 22:12:39 -0600

    Version 1.0.0 release ready

---
+++ README.md
@@ -0,0 +1,90 @@
+# AjaxTap
+
+[![npm version](https://badge.fury.io/js/ele.svg)](//npmjs.com/package/@stegopop/ajax-tap)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Listen in on any trusted XHR's made on your webpage. Run functions when your conditions are met.
+
+## Why?
+
+Say we are saving via our API. 
+
+And our API always returns this structure
+
+```
+{
+    type: String (success|failure),
+    message: String (A user-friendly message)
+}
+```
+
+In your main template you could listen to all Ajax requests, and if they have this structure and come from a URL origin you trust, you can display the message at the top of the page.
+
+```
+<script src="/dist/ajax-tap.min.js"></script>
+<script>
+    new AjaxTap()
+        .addResponseEvent({
+            conditions: function(data) {
+                return (data.type && data.message)
+            },
+            fire: function(data) {
+                var messages = document.querySelector("#messages");
+                var message = document.createElement("div");
+                    message.classList.add(data.type);
+                    message.innerText = data.message;
+                messages.appendChild(message);
+            }
+        })
+        .listen();
+</script>
+```
+
+## Install
+
+With NPM
+
+```
+npm install @stegopop/ajax-tap
+```
+
+With a CDN
+
+```
+<script src="https://cdn.jsdelivr.net/npm/@stegopop/ajax-tap"></script>
+```
+
+## Browser Support
+
+This project is transpiled to support back to IE11.
+
+## Methods
+
+`addResponseEvent(options)`
+
+Adds a Request Event to the Tap. You can add multiple Request Events to a Tap.
+This method returns the Tap so you can chain off of it.
+ 
+Request Event Object Options
+ - trustedMessengers (optional): Array
+ - conditions        (required): Function
+ - fire              (required): Function
+
+`listen()`
+
+Begin listening to ajax requests for each RequestEvent on the Tap.
+
+`url(str)`
+
+A static helper method to get url properties from a String.
+
+Returns 
+ - hostname
+ - protocol
+ - search
+ - port
+ - origin
+ - string
+ - host
+ - hash
+ - pathname
---
+++ src/AjaxTap.js
@@ -0,0 +1,107 @@
+/**
+ * AjaxTap.js
+ * 
+ * Listen in on any trusted XHR's made on your webpage.
+ * Run functions when your conditions are met.
+ * 
+ * @author Nick Adams
+ * @see {@link https://github.com/nickolasjadams/ajax-tap|Repository}
+ * @license MIT
+ * @version 1.0.0
+ */
+
+class AjaxTap {
+    
+    constructor() {
+        this.responseEvents = [];
+    }
+
+    /**
+     * Adds a Request Event to the Tap. You can add multiple Request Events to a Tap.
+     * This method returns the Tap so you can chain off of it.
+     * 
+     * Request Event
+     * - trustedMessengers (optional): Array
+     * - conditions        (required): Function
+     * - fire              (required): Function
+     * 
+     * @param Object options 
+     * @returns AjaxTap
+     */
+    addResponseEvent(options) {
+        if (!options) { throw new Error("Event options are required."); }
+        if (!options.conditions) { throw new Error("Event conditions are required."); }
+        if (!options.fire) { throw new Error("Event must have something to fire in response to met conditions."); }
+
+        let responseEvent = {
+            trustedMessengers: [],
+            conditions: null,
+            fire: null
+        };
+
+        // Add all trusted messengers
+        responseEvent.trustedMessengers.push(window.location.origin); // Default: this origin
+        if (options.trustedMessengers) {
+            options.trustedMessengers.forEach(messenger => {
+                responseEvent.trustedMessengers.push(messenger);
+            });
+        }
+
+        // Set the conditions that the response data must meet.
+        responseEvent.conditions = options.conditions;
+
+        // Set the function that will fire when this event conditions are met.
+        responseEvent.fire = options.fire;
+
+        this.responseEvents.push(responseEvent);
+
+        return this;
+    }
+
+    /**
+     * Begin listening to ajax requests for each RequestEvent on the Tap.
+     */
+    listen() {
+        if (this.responseEvents.length == 0) { throw new Error("This tap has no response events to listen to."); }
+
+        try {
+            let _this = this;
+            var origOpen = XMLHttpRequest.prototype.open;
+            XMLHttpRequest.prototype.open = function() {
+                this.addEventListener('load', function() {
+
+                    let origin = AjaxTap.url(this.responseURL).origin;
+    
+                    _this.responseEvents.forEach(e => {
+                        if (e.trustedMessengers.some( messenger => messenger == origin )) {
+                            
+                            let data = JSON.parse(this.responseText);
+
+                            if (e.conditions(data)) {
+                                e.fire(data);
+                            }
+                        }
+                    });
+                });
+                origOpen.apply(this, arguments);
+            };
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    /**
+     * Get url properties from a String.
+     * @returns {{hostname, protocol, search, port, origin: string, host, hash, pathname}}
+     */
+    static url(str) {
+        let a = document.createElement('a');
+        a.setAttribute('href', this);
+        let origin = a.protocol + '//' + a.hostname;
+        if (a.port.length > 0) {
+            origin = `${origin}:${a.port}`;
+        }
+        let {host, hostname, pathname, port, protocol, search, hash} = a;
+        return {origin, host, hostname, pathname, port, protocol, search, hash};
+    }
+}
